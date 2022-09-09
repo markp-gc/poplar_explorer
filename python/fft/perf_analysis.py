f794120b537849ce4d9326b7879463fcbca9ef1d
Mark Pupilli <markp@graphcore.ai>
2022-09-09 10:33:54 +0000

    Robustify FFT sweep, analysis, and ploting scripts.

--- python/fft/perf_analysis.py
+++ python/fft/perf_analysis.py
@@ -22,18 +22,21 @@ def getFFTInfoFromLog(log_file):
   cycles_regx = re.compile(r"FFT completed in ([-+]?[0-9]+) cycles.")
   flop_regx = re.compile(r"estimated FLOP count: ([-+]?[0-9]+)")
   matmul_regx = re.compile(r"DFT Re-Matmul shape: [-+]?[0-9]+ [-+]?[0-9]+  x [-+]?[0-9]+ ([-+]?[0-9]+)")
+  serialise_regx = re.compile(r"Serialised FFT input shape: [-+]?[0-9]+ [-+]?[0-9]+  serialisation-factor: ([-+]?[0-9]+)")
   input_size = None
   batch_size = None
   radix_size = None
   fft_cycles = None
   flops = None
   dft_batch_size = None
+  serialisation = 1
   with open(args.log_file) as f:
     for line in f:
         match1 = sizes_regx.search(line)
         match2 = cycles_regx.search(line)
         match3 = flop_regx.search(line)
         match4 = matmul_regx.search(line)
+        match5 = serialise_regx.search(line)
         if match1:
           fft_type = match1.group(1)
           input_size = match1.group(2)
@@ -45,14 +48,16 @@ def getFFTInfoFromLog(log_file):
           flops = match3.group(1)
         if match4:
           dft_batch_size = match4.group(1)
+        if match5:
+          serialisation = match5.group(1)
 
   if input_size is None:
     raise RuntimeError("Could not parse log file.")
 
   if fft_cycles is None or flops is None:
     # Was probably out of memory or other error in this case so still return the sizes:
-    return int(fft_type), int(input_size), int(batch_size), int(radix_size), None, None, int(dft_batch_size)
-  return int(fft_type), int(input_size), int(batch_size), int(radix_size), int(fft_cycles), int(flops), int(dft_batch_size)
+    return int(fft_type), int(input_size), int(batch_size), int(radix_size), None, None, int(dft_batch_size), int(serialisation)
+  return int(fft_type), int(input_size), int(batch_size), int(radix_size), int(fft_cycles), int(flops), int(dft_batch_size), int(serialisation)
 
 
 if __name__ == "__main__":
@@ -73,24 +78,28 @@ if __name__ == "__main__":
     if args.csv_out:
       print("Writing CSV headers")
       with open(args.csv_out, "w") as f:
-        f.write(f"FFT-Type,Input-size,Batch-size,Radix-size,DFT Batch-size,Cycles,FLOPS Estimate,FLOPS/Cycle,GFLOPS/sec,Memory Including Gaps (bytes),Peak Live Memory Step, Peak Live Memory (bytes)\n")
+        f.write(f"FFT-Type,Input-size,Batch-size,Radix-size,DFT Batch-size,Cycles,FLOPS Estimate,FLOPS/Cycle,GFLOPS/sec,Memory Including Gaps (bytes),Peak Live Memory Step,Peak Live Memory (bytes),Serial-steps\n")
       exit()
     else:
       raise RuntimeError("Can't write headers: no output file specified.")
 
-  report = pva.openReport(args.report_file)
-  print(f"Poplar Version: {report.poplarVersion.string}")
+  try:
+    report = pva.openReport(args.report_file)
+    print(f"Poplar Version: {report.poplarVersion.string}")
+    total_memory = sum(tile.memory.total.includingGaps for tile in report.compilation.tiles)
+    print(f"Total memory use (bytes): {total_memory}")
+    peak_name, peak_live_memory = getPeakLivenessStep(report)
+    print(f"Program step consuming peak memory: {peak_name} {peak_live_memory}")
+    # Replace commas in peak name:
+    peak_name = peak_name.replace(',',';')
+    if not peak_name:
+      peak_name = 'unknown'
+  except:
+    total_memory = 'unknown'
+    peak_name = 'unknown'
+    peak_live_memory = 'unknown'
 
-  total_memory = sum(tile.memory.total.includingGaps for tile in report.compilation.tiles)
-  print(f"Total memory use (bytes): {total_memory}")
-
-  peak_name, peak_live_memory = getPeakLivenessStep(report)
-  print(f"Program step consuming peak memory: {peak_name} {peak_live_memory}")
-
-  # Replace commas in peak name:
-  peak_name = peak_name.replace(',',';')
-
-  fft_type, size, bs, radix, cycles, flops, dft_batch_size = getFFTInfoFromLog(args.log_file)
+  fft_type, size, bs, radix, cycles, flops, dft_batch_size, serial_steps = getFFTInfoFromLog(args.log_file)
   flops_per_cycle = flops/cycles if flops else None
   gflops_per_second = flops_per_cycle * args.clock_speed_ghz if flops_per_cycle else None
   print(f"FFT type: {fft_type}D")
@@ -102,8 +111,9 @@ if __name__ == "__main__":
   print(f"DFT batch size: {dft_batch_size}")
   print(f"FLOPS per cycle: {flops_per_cycle}")
   print(f"GFLOPS/sec: {gflops_per_second}")
+  print(f"Serial steps: {serial_steps}")
 
   # Collate everything into one line of CSV and append to file if specififed:
   if args.csv_out:
     with open(args.csv_out, "a") as f:
-      f.write(f"{fft_type}D,{size},{bs},{radix},{dft_batch_size},{cycles},{flops},{flops_per_cycle},{gflops_per_second},{total_memory},{peak_name},{peak_live_memory}\n")
+      f.write(f"{fft_type}D,{size},{bs},{radix},{dft_batch_size},{cycles},{flops},{flops_per_cycle},{gflops_per_second},{total_memory},{peak_name},{peak_live_memory},{serial_steps}\n")
--- python/fft/plot.py
+++ python/fft/plot.py
@@ -26,6 +26,8 @@ def ratio_min_max(x):
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='Plotter.')
+  parser.add_argument('--outfile', required=True, type=str,
+                    help='Filename for saving the plot.')
   parser.add_argument('--csv', required=True, type=str,
                     help='CSV file containing data to be plotted.')
   parser.add_argument('--title', required=True, type=str,
@@ -44,10 +46,19 @@ if __name__ == "__main__":
                     help='CSV column heading for colour data. If None, marker colour will not be data dependent.')
   parser.add_argument('--size-field', default=None, type=str,
                     help='CSV column heading for marker size data. If None, marker size will not be data dependent.')
+  parser.add_argument('--log-base-x', default=2, type=int,
+                    help='Choose base for x-axis log scale (a value of 0 disables log scale for this axis).')
+  parser.add_argument('--log-base-y', default=2, type=int,
+                    help='Choose base for y-axis log scale (a value of 0 disables log scale for this axis).')
+  parser.add_argument('--log-base-color', default=10, type=int,
+                    help='Choose base for color-bar log scale (a value of 0 disables log scale for the color bar data).')
   args = parser.parse_args()
 
   df = pd.read_csv(args.csv, header=0)
+  df.mask(df.eq('None'), inplace=True)
+  df.dropna(axis='index', how='any', inplace=True)
   df = df.apply(pd.to_numeric, errors="coerce")
+
   headers = df.columns.values
   print(f"Column Headers:\n{headers}\n")
 
@@ -55,10 +66,14 @@ if __name__ == "__main__":
     print(f"Computing timings based on clock speed of {args.clock_speed_ghz} GHz")
     df['Time (ms)'] = df['Cycles'] / (args.clock_speed_ghz * 1e6)
 
-  x_field = args.x_field or headers[0] # 'Input-size'
-  y_field = args.y_field or headers[1] # 'Batch-size'
-  color_field = args.color_field # 'Time (ms)'
-  size_field = args.size_field # 'Radix-size'
+  if 'Memory Including Gaps (bytes)' in df:
+    print(f"Computing timings based on clock speed of {args.clock_speed_ghz} GHz")
+    df['Memory Including Gaps (MiB)'] = df['Memory Including Gaps (bytes)'] / (1024*1024)
+
+  x_field = args.x_field or headers[0]
+  y_field = args.y_field or headers[1]
+  color_field = args.color_field
+  size_field = args.size_field
 
   plot_cols = 1
   grid_spec=None
@@ -67,10 +82,8 @@ if __name__ == "__main__":
     size_scale = args.marker_scale
     size_exp = 2
     # Avoid NaNs:
-    df[size_field] = df[size_field].astype(dtype=np.float)
     sz_min = min(df[size_field])
     sz_max = max(df[size_field])
-    df[size_field] = df[size_field].fillna(value=sz_min)
     print(f"Size min/max: {sz_min} / {sz_max}")
     # Sort data by reverse of size field so all markers are visible:
     df = df.sort_values(by=[size_field], ascending=False)
@@ -103,16 +116,18 @@ if __name__ == "__main__":
 
   plt1 = df.plot(ax=axis1,
                 kind='scatter', x=x_field, y=y_field,
-                loglog=True,
                 s=sizes,
                 c=nc, cmap=cmap, colorbar=False)
-  plt1.set_xscale('log', base=2)
-  plt1.set_yscale('log', base=2)
+  if not args.log_base_x == 0:
+    plt1.set_xscale('log', base=args.log_base_x)
+  if not args.log_base_y == 0:
+    plt1.set_yscale('log', base=args.log_base_y)
 
   if color_field:
     smap = cm.ScalarMappable(norm=color_norm, cmap=cmap)
-    formatter = ticker.LogFormatterMathtext(10)
-    cb1 = fig.colorbar(mappable=smap, ax=axis1, format=formatter, norm=color_norm)
+    formatter = ticker.LogFormatterMathtext(args.log_base_color)
+    locator = ticker.LogLocator(base=args.log_base_color)
+    cb1 = fig.colorbar(mappable=smap, ax=axis1, ticks=locator, format=formatter, norm=color_norm)
     cb1.set_label(color_field)
 
   if size_field:
@@ -121,7 +136,7 @@ if __name__ == "__main__":
     marker_values = np.unique(df[size_field])
     marker_sizes = np.unique(sizes)
     axis2.scatter(y=marker_values, x=np.zeros_like(marker_values), s=marker_sizes, color=[0.4,0.4,0.4])
-    axis2.set_yscale('log', base=2)#
+    axis2.set_yscale('log', base=2)
     if len(marker_values) < 20:
       axis2.yaxis.set_ticks(marker_values)
     axis2.yaxis.tick_right()
@@ -129,7 +144,6 @@ if __name__ == "__main__":
     axis2.set_ylabel(size_field)
     axis2.get_xaxis().set_visible(False)
     axis2.set_title(f"Marker Size Legend", size=9)
-    # print(f"val: {marker_values}")
-    # print(f"sizes: {marker_sizes}")
 
-  fig.savefig('plot.png')
+  fig.savefig(args.outfile)
+  print(f"Saved '{args.outfile}'")
